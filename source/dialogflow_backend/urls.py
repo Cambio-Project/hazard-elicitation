88e44760267a3f52fd310d22f44987f7de0b41d5
styinx <st115403@stud.uni-stuttgart.de>
2021-01-06 20:06:53 +0100

    Remove unused views and URLs

--- source/dialogflow_backend/urls.py
+++ source/dialogflow_backend/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-
 from . import views
 
 
 urlpatterns = [
-    path('test/', views.test),
-    path('bot/', views.bot),
-    path('dialogflow-webhook/', csrf_exempt(views.dialogflow_webhook))
+    path('bot/', views.bot)
 ]
