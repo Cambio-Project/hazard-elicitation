0834e4475cbea279b6b30031e20a5a565b3b38eb
styinx <st115403@stud.uni-stuttgart.de>
2021-01-12 10:38:19 +0100

    Move websocket to chat.js

--- source/static/js/chat.js
+++ source/static/js/chat.js
@@ -1,12 +1,91 @@
+class CustomWebSocket {
+    static WS = null;
+
+    constructor(ws_name) {
+        CustomWebSocket.WS = this;
+        this.socket        = new WebSocket("ws://" + window.location.host + "/" + ws_name);
+
+        this.socket.onopen    = this.onOpen
+        this.socket.onclose   = this.onClose
+        this.socket.onerror   = this.onError
+        this.socket.onmessage = this.onMessage
+    }
+
+    onOpen(e) {
+        debug('Open Websocket ', e)
+    }
+
+    onClose(e) {
+        debug('Close Websocket ', e)
+    }
+
+    onError(e) {
+        debug('Error Websocket ', e)
+    }
+
+    onMessage(e) {
+        debug('On Message ', e)
+
+        const message = JSON.parse(e.data);
+
+        if (message.type in CustomWebSocket.WS) {
+            CustomWebSocket.WS[message.type](message.data);
+        } else {
+            warn("No handler function for '" + message.type + "'.")
+        }
+    }
+}
+
+class DFWebSocket extends CustomWebSocket {
+    constructor() {
+        super("ws/df/");
+    }
+
+    send(data) {
+        chat.setPending();
+        this.socket.send(JSON.stringify({
+            'type': 'dialogflow_request',
+            'data': data
+        }))
+    }
+
+    dialogflow_response(data) {
+        chat.removePending();
+        for (const i in data) {
+            const type = data[i].type;
+            const payload = data[i].payload;
+
+            switch (type) {
+                case 'text':
+                    chat.add(new ChatMessage(Chat.Bot, payload));
+                    break;
+                case 'quick_reply':
+                    chat.add(new ChatQuickReply(payload));
+                    break;
+                case 'card':
+                    chat.add(new ChatCard(payload));
+                    break;
+                case 'accordion':
+                    chat.add(new ChatAccordion(payload));
+                    break;
+                default:
+                    debug("Unknown data type '" + type + "'.")
+            }
+        }
+        chat.scroll();
+    }
+}
+
+
 class Chat {
     static User = "user";
     static Bot  = "bot";
     static Rich = "rich";
     static CHAT = null;
 
-    constructor(ws, chat_id, chat_input_id) {
+    constructor(chat_id, chat_input_id) {
         Chat.CHAT       = this;
-        this.ws         = ws;
+        this.ws         = new DFWebSocket();
         this.chat       = document.getElementById(chat_id);
         this.chat_input = document.getElementById(chat_input_id);
         this.chat_input.addEventListener("keyup", this.send);
