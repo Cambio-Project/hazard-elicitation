e6e97215debaf397f48f2710dfcad3e0eb620725
styinx <zornchrisjo@googlemail.com>
2021-02-11 10:32:03 +0100

    Move response handler into separate file and rename responses

---
+++ source/dialogflow_backend/dialogflow/response_handler.py
@@ -0,0 +1,39 @@
+from google.api_core.exceptions import InvalidArgument
+
+from dialogflow_backend.dialogflow.intents import INTENT_HANDLERS, TextMessage, INTENT_PROCESSING_ERROR
+from util.log import error, warning
+from util.text.text import text
+
+
+async def create_response(result):
+    """
+    Gets a detected intent and calls the intent handler of the corresponding intent.
+    The intent handler will produce either a text message (chat message, rich content)
+    or a non-text message (empty response, action response).
+    @param result:
+    """
+    response_data = None
+    intent = result.query_result.intent.display_name
+
+    print(result.query_result)
+
+    if intent in INTENT_HANDLERS:
+        try:
+            # Call intent handler by intent name.
+            response_data = await INTENT_HANDLERS[intent](result)
+        except InvalidArgument as e:
+            error('Intent handler for "{}" produced invalid argument: {}'.format(intent, e))
+    elif result.query_result.action:
+        response = TextMessage()
+        response.text = result.query_result.fulfillment_text
+        response_data = [response.__repr__()]
+    else:
+        warning('No intent handler found for "{}".'.format(intent))
+
+    # Create default response.
+    if not response_data:
+        response = TextMessage()
+        response.text = text(INTENT_PROCESSING_ERROR)
+        response_data = [response.__repr__()]
+
+    return response_data
--- source/dialogflow_backend/dialogflow/websocket.py
+++ source/dialogflow_backend/dialogflow/websocket.py
@@ -1,57 +1,34 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
-from google.api_core.exceptions import InvalidArgument
 
 from dialogflow_backend.dialogflow.intent_handler import *
 from dialogflow_backend.dialogflow.client import DialogFlowClient
-from dialogflow_backend.dialogflow.intents import INTENT_HANDLERS
-from util.log import info, warning, error, debug
+from dialogflow_backend.dialogflow.response_handler import create_response
+from util.log import info, warning, error
 from util.tracing import add_trace
 
 
 class DFWebsocket(AsyncWebsocketConsumer):
-    @staticmethod
-    def debug(result):
-        intent = result.query_result.intent.display_name
-        action = result.query_result.action
-        all_parameters_present = result.query_result.all_required_params_present
-        contexts = result.query_result.output_contexts
-        parameters = result.query_result.parameters
-        confidence = result.query_result.intent_detection_confidence
-        sentiment_score = result.query_result.sentiment_analysis_result.query_text_sentiment.score
-        sentiment_magnitude = result.query_result.sentiment_analysis_result.query_text_sentiment.magnitude
-
-        msg = str('Intent processing result:\n'
-                  '- Name: {}\n'
-                  '- Confidence: {}\n'
-                  '- Contexts: {}\n'
-                  '- Action: {}\n'
-                  '- All parameters present: {}\n'
-                  '- Parameters: {}\n'
-                  '- Sentiment (Score/Magnitude): {}/{}').format(
-            intent,
-            confidence,
-            contexts,
-            action,
-            all_parameters_present,
-            parameters,
-            sentiment_score,
-            sentiment_magnitude,
-        )
-
-        info(msg)
-
     async def connect(self):
+        """
+        Accept WS connection.
+        """
         await self.channel_layer.group_add('dialogflow', self.channel_name)
 
         await self.accept()
         info('Accept connection.')
 
     async def disconnect(self, close_code):
+        """
+        Disconnect WS connection
+        """
         await self.channel_layer.group_discard('dialogflow', self.channel_name)
 
         info('Disconnect connection.')
 
     async def receive(self, **kwargs):
+        """
+        WS has send data.
+        """
         # Extract text data from the message.
         data = json.loads(kwargs.get('text_data'))
 
@@ -70,39 +47,6 @@ class DFWebsocket(AsyncWebsocketConsumer):
             return
 
         await getattr(self, msg_type)(data.get('data'), data.get('contexts'))
-
-    @add_trace(True)
-    async def response_handler(self, result):
-        """
-        Gets a detected intent and calls the intent handler of the corresponding intent.
-        The intent handler will produce either a text message (chat message, rich content)
-        or a non-text message (empty response, action response).
-        @param result:
-        """
-        response_data = None
-        intent = result.query_result.intent.display_name
-
-        DFWebsocket.debug(result)
-
-        if intent in INTENT_HANDLERS:
-            try:
-                # Call intent handler by intent name.
-                response_data = await INTENT_HANDLERS[intent](result)
-            except InvalidArgument as e:
-                error('Intent handler for "{}" produced invalid argument: {}'.format(intent, e))
-        else:
-            warning('No intent handler found for "{}".'.format(intent))
-
-        # Create default response.
-        if not response_data:
-            response = TextMessage()
-            response.text = text(INTENT_PROCESSING_ERROR)
-            response_data = [response.__repr__()]
-
-        await self.send(json.dumps({
-            'type': 'dialogflow_response',
-            'data': response_data
-        }))
 
     @add_trace(True)
     async def dialogflow_text_input(self, data: str, contexts: list):
@@ -128,3 +72,16 @@ class DFWebsocket(AsyncWebsocketConsumer):
             await self.response_handler(DialogFlowClient.detect_event(data, contexts))
         except Exception as e:
             error('Something went wrong during event input processing: {}'.format(e))
+
+    @add_trace(True)
+    async def response_handler(self, result):
+        """
+        Gets a DF intent detection and creates a response message for the WS connection.
+        @param result:  DF result of intent detection.
+        """
+        response_data = await create_response(result)
+
+        await self.send(json.dumps({
+            'type': 'dialogflow_response',
+            'data': response_data
+        }))
