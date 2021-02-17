a4db03f9ede6195a52f9bfdebd90876fee52631a
styinx <st115403@stud.uni-stuttgart.de>
2021-02-17 09:28:55 +0100

    Add django restframework

--- source/hazard_elicitation/urls.py
+++ source/hazard_elicitation/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from architecture_extraction_backend.models import ArchitectureModelViewSet
 from . import views
+
+
+router = routers.DefaultRouter()
+router.register(r'architecture_models', ArchitectureModelViewSet, basename='architecture_models')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +17,8 @@ urlpatterns = [
 
     path('', views.ui),
     path('ui/', views.ui),
+    path('upload/', views.upload),
+
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
