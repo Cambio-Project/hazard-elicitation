32ba5ca4a97301d81b2bfab090955d3e914d91d1
styinx <st115403@stud.uni-stuttgart.de>
2021-02-17 16:25:07 +0100

    Add architecture list to api

--- source/architecture_extraction_backend/models.py
+++ source/architecture_extraction_backend/models.py
@@ -6,9 +6,6 @@ class ArchitectureModel(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     content = models.CharField(max_length=1024**2, null=False, blank=False)
 
-    def __str__(self):
-        return f'{self.name}'
-
 
 class ArchitectureModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -16,14 +13,32 @@ class ArchitectureModelSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'name', 'content')
 
 
+class ArchitectureModelListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ArchitectureModel
+        fields = ('id', 'name')
+
+
 class ArchitectureModelViewSet(viewsets.ModelViewSet):
     serializer_class = ArchitectureModelSerializer
 
     def get_queryset(self):
         queryset = ArchitectureModel.objects.all().order_by('id')
+        _id = self.request.query_params.get('id')
         name = self.request.query_params.get('name')
 
-        if name:
+        if _id:
+            queryset = queryset.filter(id=_id)
+        elif name:
             queryset = queryset.filter(name=name).order_by('id')
+
+        return queryset
+
+
+class ArchitectureModelListViewSet(viewsets.ModelViewSet):
+    serializer_class = ArchitectureModelListSerializer
+
+    def get_queryset(self):
+        queryset = ArchitectureModel.objects.all().order_by('name')
 
         return queryset
