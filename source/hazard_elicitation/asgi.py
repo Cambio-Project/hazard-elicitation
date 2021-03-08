d4e9560692e187dff63b9e5e280c53aa408a5f1b
styinx <st115403@stud.uni-stuttgart.de>
2021-03-08 09:00:08 +0100

    Fix import order of rest framework in asgi application

--- source/hazard_elicitation/asgi.py
+++ source/hazard_elicitation/asgi.py
@@ -1,13 +1,13 @@
 import os
 
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
-import dialogflow_backend.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hazard_elicitation.settings')
 django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+import dialogflow_backend.routing
 
 application = ProtocolTypeRouter({
     "http":      get_asgi_application(),
