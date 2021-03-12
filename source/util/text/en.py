3587405e944b2a435d94ca8ab87f573af35708ec
styinx <st115403@stud.uni-stuttgart.de>
2021-03-12 14:17:37 +0100

    Add more intents
    - Divide elicitation into a 6 steps
    - Add stimuli, responses and measures

--- source/util/text/en.py
+++ source/util/text/en.py
@@ -1,15 +1,85 @@
 from util.text.ids import *
 
 TEXT = {
-    DEFAULT:                                                   '...',
+    DEFAULT:                                                 '...',
+
+    # Knowledge base
+    KB_TEXTS:                                                [
+        {
+            'title': 'Tracing',
+            'link':  {
+                'text': 'Learn more about tracing',
+                'url':  'https://en.wikipedia.org/wiki/Tracing_(software)'
+            },
+            'text':  str(
+                'In software engineering, tracing involves a specialized use of logging to record information about a '
+                'programs execution. This information is typically used by programmers for debugging purposes; and '
+                'additionally, depending on the type and detail of information contained in a trace log, by '
+                'experienced system administrators or technical-support personnel and by software monitoring tools to '
+                'diagnose common problems with software. Tracing is a cross-cutting concern.')
+        },
+        {
+            'title': 'Jaeger',
+            'link':  {
+                'text': 'Learn more about jaeger',
+                'url':  'https://www.jaegertracing.io/'
+            },
+            'text':  str(
+                'As on-the-ground microservice practitioners are quickly realizing, the majority of operational '
+                'problems that arise when moving to a distributed architecture are ultimately grounded in two areas: '
+                'networking and observability. It is simply an orders of magnitude larger problem to network and debug '
+                'a set of intertwined distributed services versus a single monolithic application.')
+        },
+        {
+            'title': 'Zipkin',
+            'link':  {
+                'text': 'Learn more about zipkin',
+                'url':  'https://zipkin.io/'},
+            'text':  str(
+                'Zipkin is a distributed tracing system. It helps gather timing data needed to troubleshoot latency '
+                'problems in service architectures. Features include both the collection and lookup of this data.')
+        }
+    ],
+
+    # Stimulus Responses & Response Measures
+    STIMULUS_RESPONSE_TEXTS:                                 {
+        'Service':   {
+            'stimuli':           [
+                'Decreased service performance',
+                'Service failure'
+            ],
+            'responses':         [
+                'Service should return to normal performance',
+                'Service should restart'
+            ],
+            'response-measures': {
+                'normal-time':   ['10s', '5s', '3s', '1s', '500s', '300ms', '100ms', '50ms', '30ms', '10ms', '<10ms'],
+                'normal-cases':  ['99.99%', '99.9%', '99%', '98%', '97%', '96%', '95%'],
+                'recovery-time': ['5min', '3min', '1min', '30s', '15s', '10s', '1s']
+            }},
+        'Operation': {
+            'stimuli':           [
+                'Spike response times',
+                'Response time deviations'
+            ],
+            'responses':         [
+                'Response times should return to normal.'
+            ],
+            'response-measures': {
+                'normal-time':   ['10s', '5s', '3s', '1s', '500s', '300ms', '100ms', '50ms', '30ms', '10ms', '<10ms'],
+                'normal-cases':  ['99.99%', '99.9%', '99%', '98%', '97%', '96%', '95%'],
+                'recovery-time': ['5min', '3min', '1min', '30s', '15s', '10s', '1s']
+            }},
+    },
 
     # Fallback
-    INTENT_PROCESSING_ERROR:                                   'Ops something went wrong.',
+    INTENT_PROCESSING_ERROR:                                 'Ops something went wrong.',
 
     # Default
-    INTENT_EMPTY_NAME:                                         'Default-Empty',
-    INTENT_FALLBACK_NAME:                                      'Default-Fallback',
-    INTENT_FALLBACK_TEXT:                                      [
+    INTENT_EMPTY_NAME:                                       'Default-Empty',
+    INTENT_CLEAR_NAME:                                       'Default-Clear',
+    INTENT_FALLBACK_NAME:                                    'Default-Fallback',
+    INTENT_FALLBACK_TEXT:                                    [
         'That doesn\'t compute. Maybe you can rephrase your sentence?',
         'I don\'t know what you mean. Can you elaborate?',
         'I missed what you said. What was that?',
@@ -20,18 +90,24 @@ TEXT = {
         'That doesn\'t really make sense to me.',
         'Maybe try something different?.'
     ],
-    INTENT_HELP_NAME:                                          'Default-Help',
-    INTENT_HELP_TEXT:                                          [
+    INTENT_BYE_NAME:                                         'Default-Bye',
+    INTENT_BYE_TEXT:                                         [
+        'Nice to work with you! See you around. &#x1F44B;',
+        'Okay then! Have a nice day.',
+        'Thanks for participating! See you &#x1F44B;'
+    ],
+    INTENT_HELP_NAME:                                        'Default-Help',
+    INTENT_HELP_TEXT:                                        [
         str('Hang on, help is on it\'s way!'
             'I am a chatbot that helps to elicit resilience scenarios.')
     ],
 
     # Guide
-    INTENT_GUIDE_NAME:                                         'Default-Guide',
-    INTENT_GUIDE_TEXT:                                         str(
+    INTENT_GUIDE_NAME:                                       'Default-Guide',
+    INTENT_GUIDE_TEXT:                                       str(
         'Let us go through this, step by step.<br>'
         'Please select or tell me about which topic you would like to learn more about...'),
-    INTENT_GUIDE_OPTIONS:                                      {
+    INTENT_GUIDE_OPTIONS:                                    {
         'Architecture': {
             'text':  str(
                 'An architecture is constructed from the result of a '
@@ -58,24 +134,25 @@ TEXT = {
                 'Another type of component is an edge which represents an operation of the system.'
             )}
     },
-    INTENT_GUIDE_CONTINUE_TEXT:                                [
+    INTENT_GUIDE_CONTINUE_TEXT:                              [
         'Do you want to know more?',
         'Do you need more infos?',
         'Anything else you want to know?'
     ],
-    INTENT_GUIDE_CONTINUE_CONFIRM_TEXT:                        'I am good, let\'s continue! &#x2714;',
-    INTENT_GUIDE_OPTION_NAME:                                  'Default-Guide-Option',
-    INTENT_GUIDE_CONFIRM_NAME:                                 'Default-Guide-Confirm',
+    INTENT_GUIDE_CONTINUE_CONFIRM_TEXT:                      'I am good, let\'s continue! &#x2714;',
+    INTENT_GUIDE_OPTION_NAME:                                'Default-Guide-Option',
+    INTENT_GUIDE_CONFIRM_NAME:                               'Default-Guide-Confirm',
 
     # Welcome
-    INTENT_WELCOME_NAME:                                       'Default-Welcome',
-    INTENT_WELCOME_TEXT:                                       str(
+    INTENT_WELCOME_NAME:                                     'Default-Welcome',
+    INTENT_WELCOME_TEXT:                                     str(
         'Hey there! &#x1F44B;<br><br>'
         'I am a chatbot and I will help you to elicit <b>resilience scenarios</b>. '
         'For each scenario we will go through the following steps:<br>'
         '<ol>'
         '<li>Select an <b>architecture</b> to analyze</li>'
         '<li>Select a <b>component</b> of the architecture</li>'
+        '<li>Specify a <b>stimuli</b></li>'
         '<li>Specify a <b>response</b></li>'
         '<li>Specify a <b>response measure</b></li>'
         '<li>Save the <b>resilience scenario</b></li>'
@@ -84,58 +161,84 @@ TEXT = {
         'or configure the scenario in the user interface.<br><br>'
         'Everything clear? Are you ready?'
     ),
-    INTENT_WELCOME_RESUME_TEXT:                                'Continue where you left. &#x21bb;',
-    INTENT_WELCOME_YES_TEXT:                                   'Yes, let\'s go! &#x1F44D;',
-    INTENT_WELCOME_NO_TEXT:                                    'No, I need more information. &#x2753;',
-    INTENT_WELCOME_CONFIRM_NAME:                               'Default-Welcome-Confirm',
-    INTENT_WELCOME_DECLINE_NAME:                               'Default-Welcome-Decline',
+    INTENT_WELCOME_RESUME_TEXT:                              'Continue where you left. &#x21bb;',
+    INTENT_WELCOME_YES_TEXT:                                 'Yes, let\'s go! &#x1F44D;',
+    INTENT_WELCOME_NO_TEXT:                                  'No, I need more information. &#x2753;',
+    INTENT_WELCOME_CONFIRM_NAME:                             'Default-Welcome-Confirm',
+    INTENT_WELCOME_DECLINE_NAME:                             'Default-Welcome-Decline',
 
     # Elicitation
-    INTENT_ELICITATION_ARCHITECTURE_NAME:                      'Elicitation-Select-Architecture',
-    INTENT_ELICITATION_ARCHITECTURE_TEXT:                      {
+    INTENT_ELICITATION_ARCHITECTURE_NAME:                    'Elicitation-Select-Architecture',
+    INTENT_ELICITATION_ARCHITECTURE_TEXT:                    {
         'title': 'Step 1 - Select an architecture',
         'text':  'Below you are given a list of architectures. Please select one architecture.'
     },
-    INTENT_ELICITATION_COMPONENT_NAME:                         'Elicitation-Select-Component',
-    INTENT_ELICITATION_COMPONENT_TEXT:                         {
+    INTENT_ELICITATION_ARCHITECTURE_FOLLOWUP_NAME:           'Elicitation-Select-Architecture-Followup',
+    INTENT_ELICITATION_COMPONENT_NAME:                       'Elicitation-Select-Component',
+    INTENT_ELICITATION_COMPONENT_TEXT:                       {
         'title': 'Step 2 - Select a component from <i>{}</i>',
         'text':  'Please select one component from the following selection.'
     },
-    INTENT_ELICITATION_COMPONENT_SERVICE_TEXT:                 'Here are the services to choose from ...',
-    INTENT_ELICITATION_COMPONENT_OPERATION_TEXT:               'Here are the operations to choose from ...',
-    INTENT_ELICITATION_RESPONSE_NAME:                          'Elicitation-Specify-Response',
-    INTENT_ELICITATION_RESPONSE_TEXT:                          {
-        'title': 'Step 3 - Specify response for <i>{}</i>',
-        'text':  'A response is ...'
+    INTENT_ELICITATION_COMPONENT_SERVICE_TEXT:               'Here are the <b>services</b> to choose from ...',
+    INTENT_ELICITATION_COMPONENT_OPERATION_TEXT:             'Here are the <b>operations</b> to choose from ...',
+    INTENT_ELICITATION_COMPONENT_FOLLOWUP_NAME:              'Elicitation-Select-Component-Followup',
+    INTENT_ELICITATION_STIMULUS_NAME:                        'Elicitation-Specify-Stimulus',
+    INTENT_ELICITATION_STIMULUS_TEXT:                        {
+        'title': 'Step 3 - Specify a stimuli for <i>{}</i>',
+        'text':  'What is a potential hazard for this {}?'
     },
-    INTENT_ELICITATION_RESPONSE_FOLLOWUP_NAME:                 'Elicitation-Specify-Response-Followup',
-    INTENT_ELICITATION_RESPONSE_MEASURE_NAME:                  'Elicitation-Specify-Response-Measure',
-    INTENT_ELICITATION_RESPONSE_MEASURE_TEXT:                  {
-        'title': 'Step 4 - Specify response measure for <i>{}</i>',
-        'text':  'A response is ...'
+    INTENT_ELICITATION_STIMULUS_FOLLOWUP_NAME:               'Elicitation-Specify-Stimulus-Followup',
+    INTENT_ELICITATION_RESPONSE_NAME:                        'Elicitation-Specify-Response',
+    INTENT_ELICITATION_RESPONSE_TEXT:                        {
+        'title': 'Step 4 - Specify a response for <i>{}</i>',
+        'text':  'A response defines what action should be performed against the stimuli.'
     },
-    INTENT_ELICITATION_RESPONSE_MEASURE_FOLLOWUP_NAME:         'Elicitation-Specify-Response-Measure-Followup',
-    INTENT_ELICITATION_SAVE_SCENARIO_NAME:                     'Elicitation-Save-Scenario',
-    INTENT_ELICITATION_SAVE_SCENARIO_TEXT:                     {
-        'title': 'Step 5 - Save scenario',
-        'text':  'A response is ...'
+    INTENT_ELICITATION_RESPONSE_FOLLOWUP_NAME:               'Elicitation-Specify-Response-Followup',
+    INTENT_ELICITATION_RESPONSE_MEASURE_NAME:                'Elicitation-Specify-Response-Measure',
+    INTENT_ELICITATION_RESPONSE_MEASURE_TEXT:                {
+        'title': 'Step 5 - Specify a response measure for <i>{}</i>',
+        'text':  'A response measure quantifies the response.'
     },
-
-    # Util
-    INTENT_COMMAND_CONFIG_NAME:                                'Command-Config',
-    INTENT_COMMAND_CONFIG_LIST_NAME:                           'Command-Config-List',
-    INTENT_COMMAND_MANAGE_NAME:                                'Command-Manage',
-    INTENT_COMMAND_MANAGE_LIST_NAME:                           'Command-Manage-List',
+    INTENT_ELICITATION_RESPONSE_MEASURE_NORMAL_TIME_TEXT:    'What would be an optimal response time?',
+    INTENT_ELICITATION_RESPONSE_MEASURE_NORMAL_CASES_TEXT:   'In how many cases should this hold?',
+    INTENT_ELICITATION_RESPONSE_MEASURE_RECOVERY_TIME_TEXT:  'How long is a <b>non-optimal</b> metric tolerable?',
+    INTENT_ELICITATION_RESPONSE_MEASURE_FOLLOWUP_TIMES_NAME: 'Elicitation-Specify-Response-Measure-Followup-Times',
+    INTENT_ELICITATION_RESPONSE_MEASURE_FOLLOWUP_CASES_NAME: 'Elicitation-Specify-Response-Measure-Followup-Cases',
+    INTENT_ELICITATION_SAVE_SCENARIO_NAME:                   'Elicitation-Save-Scenario',
+    INTENT_ELICITATION_SAVE_SCENARIO_TEXT:                   {
+        'title': 'Step 6 - Save the resilience scenario',
+        'text':  'The resilience scenario has now all necessary parameters configured.'
+    },
+    INTENT_ELICITATION_SUMMARY_SERVICE_MEASURE_TEXT: str(),
+    INTENT_ELICITATION_SUMMARY_OPERATION_MEASURE_TEXT: str(
+        'Normal response time is <b>{}</b> (holds in <b>{}</b> of cases).<br>'
+        'Within <b>{}</b> after occurrence of the stimuli the response times return to normal values.'),
+    INTENT_ELICITATION_SAVE_SCENARIO_CONTINUE_TEXT:          'Here is the summary of your current scenario.',
+    INTENT_ELICITATION_SAVE_SCENARIO_SAVE_TEXT:              'Is the scenario finished?',
+    INTENT_ELICITATION_SAVE_SCENARIO_SAVE_CONFIRM_TEXT:      'Yes, save the scenario. &#x2714;',
+    INTENT_ELICITATION_SAVE_SCENARIO_MODIFY_OPTIONS:         {
+        'e-select-component':         'Modify the component',
+        'e-specify-stimuli':          'Modify the stimuli',
+        'e-specify-response':         'Modify the response',
+        'e-specify-response-measure': 'Modify the response measure'
+    },
+    INTENT_ELICITATION_SAVE_SCENARIO_CONFIRM_NAME:           'Elicitation-Save-Scenario-Confirm',
+    INTENT_ELICITATION_NEXT_STEP_NAME:                       'Elicitation-Next-Step',
+    INTENT_ELICITATION_NEXT_STEP_TEXT:                       'Do you want to create another scenario?',
+    INTENT_ELICITATION_NEXT_STEP_EXIT_TEXT:                  'No &#x274C;',
+    INTENT_ELICITATION_NEXT_STEP_CONTINUE_TEXT:              'Yes &#x2714;',
+    INTENT_ELICITATION_NEXT_STEP_CONFIRM_NAME:               'Elicitation-Next-Step-Confirm',
+    INTENT_ELICITATION_NEXT_STEP_DECLINE_NAME:               'Elicitation-Next-Step-Decline',
 
     # Extra
-    INTENT_FACT_NAME:                                          'Extra-Fact',
-    INTENT_FACT_TEXT:                                          [
+    INTENT_FACT_NAME:                                        'Extra-Fact',
+    INTENT_FACT_TEXT:                                        [
         'I didn\'t know that one &#x1F446;.',
         'Hm, very interesting.',
         'Who would have thought...'
     ],
-    INTENT_JOKE_NAME:                                          'Extra-Joke',
-    INTENT_JOKE_TEXT:                                          [
+    INTENT_JOKE_NAME:                                        'Extra-Joke',
+    INTENT_JOKE_TEXT:                                        [
         'Yeah, that\'s a good one &#x1F604;.',
         'Yikes &#x1F923;.',
         'Not so sure about that one &#x1F928;.'
