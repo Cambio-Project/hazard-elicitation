71d64a1d0234aa7ae395fa8682b2b0e29c587067
styinx <st115403@stud.uni-stuttgart.de>
2021-05-27 21:06:52 +0200

    Remove study flag

--- source/hazard_elicitation/settings.py
+++ source/hazard_elicitation/settings.py
@@ -10,7 +10,7 @@ mimetypes.add_type("text/javascript", ".js", True)
 ALLOWED_HOSTS = ['*']
 BASE_DIR = path(__file__, '..', '..')
 CONFIG = Config(path(BASE_DIR, '..', 'config.json'))
-COLLECT_STUDY = True
+COLLECT_STUDY = False
 CSRF_COOKIE_SECURE = True
 DEBUG = False
 FORMATTED_LOGGING = True
