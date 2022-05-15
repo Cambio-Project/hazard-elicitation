15f4b9e2127168f0252428133af280fd8057aa14
Scotcho <hafner.marcel@gmx.de>
2022-05-15 04:00:53 -0700

    remove old folder

--- source/architecture_extraction_backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models.architecture import ArchitectureModel
-from .models.study import InteractionModel, ScenarioModel
-
-# Register your models here.
-admin.site.register(ArchitectureModel)
-admin.site.register(InteractionModel)
-admin.site.register(ScenarioModel)
